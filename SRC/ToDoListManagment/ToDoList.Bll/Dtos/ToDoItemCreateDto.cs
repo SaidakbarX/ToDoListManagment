c0a4076e9f2c5b69f3a9abc4b333e09b76787ab1
mrbuzb <mrbpubg14@gmail.com>
2025-04-12 00:23:11 -0700

    Dtos Added

---
+++ SRC/ToDoListManagment/ToDoList.Bll/Dtos/ToDoItemCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Bll.Dtos;
+
+public class ToDoItemCreateDto
+{
+    public string Title { get; set; }
+    public string Discription { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
