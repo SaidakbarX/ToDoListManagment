11400771151dd733ca1a09228ccdf6016672048c
mrbuzb <mrbpubg14@gmail.com>
2025-04-12 00:09:17 -0700

    Dtos Added

--- SRC/ToDoListManagment/ToDoList.Bll/Dtos/ToDoItemUpdateDto.cs
+++ SRC/ToDoListManagment/ToDoList.Bll/Dtos/ToDoItemUpdateDto.cs
@@ -10,7 +10,7 @@ public class ToDoItemUpdateDto
 {
     public long Id { get; set; }
     public string Title { get; set; }
-    public string Discription { get; set; }
+    public string Discription { get; set; } 
     public bool IsCompleted { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime DueDate { get; set; }
