04af2019ff841136dbeb08950791437822e3f110
Otabek <totabek006@gmail.com>
2025-04-12 13:34:04 +0300

    chalasi bor

--- SRC/ToDoListManagment/ToDoList.Repository/Services/IToDoItemRepository.cs
+++ SRC/ToDoListManagment/ToDoList.Repository/Services/IToDoItemRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace ToDoList.Repository.Services
+﻿using ToDoList.DAL.Entity;
+
+namespace ToDoList.Repository.Services
 {
-    internal interface IToDoItemRepository
+    public interface IToDoItemRepository
     {
+        Task<long> InsertToDoItemAsync(long id);
+        Task DeleteToDoItemByIdAsync(long id);
+        Task<ToDoItem> UpdateToDoItemAsync(ToDoItem item);
+        Task<List<ToDoItem>> SelectAllToDoItemsAsync(int skip, int take)
+ SelectToDoItemByIdAsync()
+ SelectByDueDateAsync
+ SelectCompletedAsync(skip, take)
+            SelectIncompleteAsync(skip, take)
     }
 }
