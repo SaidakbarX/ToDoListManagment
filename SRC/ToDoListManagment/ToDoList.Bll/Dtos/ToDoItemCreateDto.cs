54d158e23d5a403da845bef0b561a21ec272a495
mrbuzb <mrbpubg14@gmail.com>
2025-04-12 00:02:03 -0700

    DtosCreated

--- SRC/ToDoListManagment/ToDoList.Bll/Dtos/ToDoItemCreateDto.cs
+++ SRC/ToDoListManagment/ToDoList.Bll/Dtos/ToDoItemCreateDto.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ToDoList.DAL.Entity;
+namespace ToDoList.Bll.Dtos;
 
-public class ToDoItem
+public class ToDoItemCreateDto
 {
-    public long Id { get; set; }
     public string Title { get; set; }
     public string Discription { get; set; }
     public bool IsCompleted { get; set; }
     public DateTime CreatedAt { get; set; }
-    public DateTime DueDate { get; set; }
 }
--- SRC/ToDoListManagment/ToDoList.Bll/Dtos/ToDoItemUpdateDto.cs
+++ SRC/ToDoListManagment/ToDoList.Bll/Dtos/ToDoItemUpdateDto.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ToDoList.DAL.Entity;
+namespace ToDoList.Bll.Dtos;
 
-public class ToDoItem
+public class ToDoItemUpdateDto
 {
     public long Id { get; set; }
     public string Title { get; set; }
